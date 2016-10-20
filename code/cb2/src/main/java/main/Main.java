241d9a3a3af56cd9dd53e25bc2e4ee9b548f216c
Simon Hanna <simon.hanna@jesus.de>
2016-10-20 20:02:12 +0200

    Add gradle project (#1)
    * Use gradle project for easy testing
    
    * Add travis config

---
+++ code/cb2/src/main/java/main/Main.java
@@ -0,0 +1,11 @@
+package main;
+public class Main {
+	
+    public boolean someLibraryMethod() {
+        return true;
+    }
+    
+    public static void main(String... args) {
+    	System.out.println("Hello World!");
+    }
+}
