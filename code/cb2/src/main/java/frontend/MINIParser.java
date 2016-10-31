24eec8d31c2d2276c0bb956b48ee34b8f21f4c9c
Simon Hanna <simon.h4nn4@gmail.com>
2016-10-31 16:32:59 +0100

    Add method to start prettyPrinting

--- code/cb2/src/main/java/frontend/MINIParser.java
+++ code/cb2/src/main/java/frontend/MINIParser.java
@@ -1,12 +1,18 @@
 package frontend;
 
 import java.io.File;
+import java.util.ArrayList;
 
+import components.ClassNode;
 import parser.MINIGrammar;
 import testsuite.MINIException;
 
 public class MINIParser {
 	public static void wortproblem (File in) throws MINIException {
 		MINIGrammar.parse(in);
+	}
+
+	public static void prettyPrinted(File in, File out) throws MINIException {
+	    ArrayList<ClassNode> classes = MINIGrammar.parse(in);
 	}
 }
