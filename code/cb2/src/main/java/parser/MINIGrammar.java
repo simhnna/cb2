cae2f7828ca165d27a34716dd402416b6db1f05f
Daniel Nägele <code@danieln.de>
2016-10-31 14:25:41 +0100

    remove unnecessary vars

--- code/cb2/src/main/java/parser/MINIGrammar.java
+++ code/cb2/src/main/java/parser/MINIGrammar.java
@@ -49,12 +49,12 @@ public class MINIGrammar implements MINIGrammarConstants {
     throw new Error("Missing return statement in function");
   }
 
-  final public ClassNode mini_class() throws ParseException {
+  final public classNode mini_class() throws ParseException {
   ClassNode cls = new ClassNode();
-  Token clsName;
   Node classMember;
     jj_consume_token(CLASS);
-    clsName = jj_consume_token(ID);
+    jj_consume_token(ID);
+           cls.name = token.image;
     jj_consume_token(BRACE_OPEN);
     label_2:
     while (true) {
@@ -70,7 +70,6 @@ public class MINIGrammar implements MINIGrammarConstants {
       cls.children.add(classMember);
     }
     jj_consume_token(BRACE_CLOSE);
-    cls.name = clsName.image;
     {if (true) return cls;}
     throw new Error("Missing return statement in function");
   }
