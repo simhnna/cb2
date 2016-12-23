05c63eac8a16c3374e8dd597e5bc71554573f215
Simon Hanna <simon.h4nn4@gmail.com>
2016-12-23 01:56:53 +0100

    Remove useless global NameTable

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -12,14 +12,13 @@ import visitors.NameAndTypeChecker;
 public class Main {
 
     public static void main(String... args) {
-        NameTable globalNameTable = new NameTable(null, null);
         FileNode classes;
         try {
             File sourceFile = new File("res" + File.separator + "example_code" + File.separator + "valid"
                     + File.separator + "pretty_much_everything.m");
             classes = MINIGrammar.parse(sourceFile);
             NameAndTypeChecker checker = new NameAndTypeChecker();
-            classes.accept(checker, globalNameTable);
+            classes.accept(checker, null);
             JavaCodeGenerator javaPrinter = new JavaCodeGenerator();
             classes.accept(javaPrinter, null);
             System.out.println(javaPrinter.toString());
