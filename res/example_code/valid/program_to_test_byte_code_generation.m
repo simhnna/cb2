7c61244658c4d8029d2a32757fea42645ab435dd
Simon Hanna <simon.h4nn4@gmail.com>
2017-01-08 16:39:05 +0100

    Update code used to test bytecodegeneration

--- res/example_code/valid/program_to_test_byte_code_generation.m
+++ res/example_code/valid/program_to_test_byte_code_generation.m
@@ -1,6 +1,8 @@
 class a {
   void foo() {
     var foo := new<int[], 100>;
+    "should be 100";
+    foo.size();
     foo.set(1, 100);
     foo.get(2);
   }
@@ -8,10 +10,28 @@ class a {
   int bar(int i) {
     return i - 1;
   }
+}
 
+class main {
   void main(string[] args) {
     var m := new<a>;
     m.print();
     m.bar(1).print();
+    m.bar(0);
+    if (m.bar(0) != -1) {
+      "false".print();
+    }
+    "asdf".size();
+    "asdf".print();
+    if ("asdf".size() != 4) {
+      "false".print();
+    } else {
+      "true".print();
+    }
+    if ("asdf".size() == 4) {
+      "true".print();
+    } else {
+      "false".print();
+    }
   }
 }
